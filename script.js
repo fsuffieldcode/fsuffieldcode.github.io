d18d3980ccf36c58cbcc5ab80ac6b52a86cab24d
fsuffieldcode <fsuffieldcode@gmail.com>
2019-09-05 01:26:19 +0100

    add contact page

---
+++ script.js
@@ -0,0 +1,12 @@
+// FOCUS RING
+
+function handleFirstTab(e) {
+    if (e.keyCode === 9) { // the "I am a keyboard user" key
+        document.body.classList.add('user-is-tabbing');
+        window.removeEventListener('keydown', handleFirstTab);
+    }
+}
+
+window.addEventListener('keydown', handleFirstTab);
+
+// FOCUS RING
